1d570c9616814c07885ae98d5575bb733c358b6d
H.R.Shadhin <hrshadhin.i386@gmail.com>
2018-06-02 17:58:50 +0600

    add trait for model

---
+++ src/UserstampsTrait.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Hrshadhin\Userstamps;
+
+use App\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * Class UserstampsTrait
+ *
+ * @property int created_by
+ * @property int updated_by
+ * @property int deleted_by
+ * @method   string getTable()
+ * @method   void observe($observer)
+ * @method   BelongsTo belongsTo($related, $foreignKey = null, $otherKey = null, $relation = null)
+ * @package  Hrshadhin\Userstamps
+ */
+
+trait UserstampsTrait
+{
+    /**
+     * Boot the userstamps trait for a model.
+     *
+     * @return void
+     */
+    public static function bootUserstampsTrait()
+    {
+        static::observe(new UserstampsTraitObserver);
+    }
+
+    /**
+     * Get user model who created the record.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function creator()
+    {
+        return $this->belongsTo($this->getUserInstance(), $this->getCreatedByColumn());
+    }
+
+    /**
+     * Get column name for created by.
+     *
+     * @return string
+     */
+    protected function getCreatedByColumn()
+    {
+        return 'created_by';
+    }
+
+    /**
+     * Get user model who updated the record.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function updater()
+    {
+        return $this->belongsTo($this->getUserInstance(), $this->getUpdatedByColumn());
+    }
+
+    /**
+     * Get column name for updated by.
+     *
+     * @return string
+     */
+    protected function getUpdatedByColumn()
+    {
+        return 'updated_by';
+    }
+
+    /**
+     * Get user model who deleted the record.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function deletor()
+    {
+        return $this->belongsTo($this->getUserInstance(), $this->getDeletedByColumn());
+    }
+
+    /**
+     * Get column name for deleted by.
+     *
+     * @return string
+     */
+    protected function getDeletedByColumn()
+    {
+        return 'deleted_by';
+    }
+
+    /**
+     * Get created by user name.
+     *
+     * @return string
+     */
+    public function getCreatedByNameAttribute()
+    {
+        if ($this->{$this->getCreatedByColumn()}) {
+            $user = $this->getUserInstance()->find($this->{$this->getCreatedByColumn()});
+
+            return $user->name;
+        }
+
+        return '';
+    }
+
+    /**
+     * Get Laravel's user class instance.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getUserInstance()
+    {
+        $class = config('auth.providers.users.model', User::class);
+
+        return new $class;
+    }
+
+    /**
+     * Get updated by user name.
+     *
+     * @return string
+     */
+    public function getUpdatedByNameAttribute()
+    {
+        if ($this->{$this->getUpdatedByColumn()}) {
+            $user = $this->getUserInstance()->find($this->{$this->getUpdatedByColumn()});
+
+            return $user->name;
+        }
+
+        return '';
+    }
+
+    /**
+     * Get deleted by user name.
+     *
+     * @return string
+     */
+    public function getDeletedByNameAttribute()
+    {
+        if ($this->{$this->getDeletedByColumn()}) {
+            $user = $this->getUserInstance()->find($this->{$this->getDeletedByColumn()});
+
+            return $user->name;
+        }
+
+        return '';
+    }
+    
+}
---
+++ src/UserstampsTraitObserver.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Hrshadhin\Userstamps;
+
+/**
+ * Class Userstamps
+ *
+ * @package Hrshadhin\Userstamps
+ */
+class UserstampsTraitObserver
+{
+    /**
+     * Model's creating event hook.
+     *
+     * @param \Hrshadhin\Userstamps\UserstampsTrait $model
+     */
+    public function creating($model)
+    {
+        if (! $model->created_by) {
+            $model->created_by = $this->getAuthenticatedUserId();
+        }
+
+        if (! $model->updated_by) {
+            $model->updated_by = $this->getAuthenticatedUserId();
+        }
+       
+    }
+
+    /**
+     * Get authenticated user id depending on model's auth guard.
+     *
+     * @return int
+     */
+    protected function getAuthenticatedUserId()
+    {
+        return auth()->check() ? auth()->id() : 0;
+    }
+
+    /**
+     * Model's updating event hook.
+     *
+     * @param \Hrshadhin\Userstamps\UserstampsTrait $model
+     */
+    public function updating($model)
+    {
+        if (! $model->isDirty('updated_by')) {
+            $model->updated_by = $this->getAuthenticatedUserId();
+        }
+    }
+
+     /**
+      * Model's deleting event hook.
+      *
+      * @param \Hrshadhin\Userstamps\UserstampsTrait $model
+      */
+    public function deleting($model)
+    {
+        if (! $model->isDirty('deleted_by')) {
+            $model->deleted_by = $this->getAuthenticatedUserId();
+        }
+    }
+
+     /**
+      * Model's restoring event hook.
+      *
+      * @param \Hrshadhin\Userstamps\UserstampsTrait $model
+      */
+    public function restoring($model)
+    {
+        if (! $model->isDirty('deleted_by')) {
+            $model->deleted_by = null;
+        }
+    }
+}
